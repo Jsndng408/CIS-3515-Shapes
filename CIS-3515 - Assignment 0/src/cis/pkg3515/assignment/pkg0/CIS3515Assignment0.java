d75250a28786e2b6e9ca973d1fcdc2daa7e9fb77
Jsndng408 <54553870+Jsndng408@users.noreply.github.com>
2020-01-26 00:02:14 -0500

    Add files via upload

--- CIS-3515 - Assignment 0/src/cis/pkg3515/assignment/pkg0/CIS3515Assignment0.java
+++ CIS-3515 - Assignment 0/src/cis/pkg3515/assignment/pkg0/CIS3515Assignment0.java
@@ -4,21 +4,42 @@
 
 package cis.pkg3515.assignment.pkg0;
 
+import java.util.*;
+
 public class CIS3515Assignment0 {
     public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        
         // Square Object Testing
-        Square square = new Square("Block");
-        square.setDimensions(10, 12);
+        System.out.println("Enter the name of a Square");
+        String squareName = scn.nextLine();
+        Square square = new Square(squareName);
+        
+        System.out.println("Enter the length of " + squareName);
+        double length = scn.nextDouble();
+        System.out.println("Enter the height of " + squareName);
+        double height = scn.nextDouble();
+        square.setDimensions(length, height);
+        scn.nextLine(); // Used to consume the new line leftover from scn.nextDouble()
+        
         System.out.println("Square Object's Name is " + square.getName());
         square.printDimensions();
+        //System.out.println("Square Object's Area = " + square.getArea());
         String squareArea = String.format("%.2f", square.getArea());
         System.out.println("Square Object's Area = " + squareArea);
         
         System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
         
         // Circle Object Testing
-        Circle circle = new Circle("Round");
-        circle.setDimensions(3);
+        System.out.println("Enter the name of a Circle");
+        String circleName = scn.nextLine();
+        Circle circle = new Circle(circleName);
+        
+        System.out.println("Enter the radius of " + circleName);
+        double radius = scn.nextDouble();
+        circle.setDimensions(radius);
+        scn.nextLine(); // Used to consume the new line leftover from scn.nextDouble()
+        
         System.out.println("Circle Object's Name is " + circle.getName());
         circle.printDimensions();
         String circleArea = String.format("%.2f", circle.getArea());
@@ -27,8 +48,19 @@ public class CIS3515Assignment0 {
         System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
         
         // Triangle Object Testing
-        Triangle triangle = new Triangle("Pointy");
-        triangle.setDimensions(3, 4, 5);
+        System.out.println("Enter the name of a Triangle");
+        String triangleName = scn.nextLine();
+        Triangle triangle = new Triangle(triangleName);
+        
+        System.out.println("Enter the length of the first side of " + triangleName);
+        double side1 = scn.nextDouble();
+        System.out.println("Enter the length of the second side of " + triangleName);
+        double side2 = scn.nextDouble();
+        System.out.println("Enter the length of the third side of " + triangleName);
+        double side3 = scn.nextDouble();
+        triangle.setDimensions(side1, side2, side3);
+        scn.nextLine(); // Used to consume the new line leftover from scn.nextDouble()
+        
         System.out.println("Triangle Object's Name is " + triangle.getName());
         triangle.printDimensions();
         String triangleArea = String.format("%.2f", triangle.getArea());
@@ -37,8 +69,15 @@ public class CIS3515Assignment0 {
         System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
         
         // Equilateral Triangle Object Testing
-        EquilateralTriangle eqTriangle = new EquilateralTriangle("Equally Pointy");
-        eqTriangle.setDimensions(4);
+        System.out.println("Enter the name of an Equilateral Triangle");
+        String eqTriangleName = scn.nextLine();
+        EquilateralTriangle eqTriangle = new EquilateralTriangle(eqTriangleName);
+        
+        System.out.println("Enter the length of the side of " + eqTriangleName);
+        double side = scn.nextDouble();
+        eqTriangle.setDimensions(side);
+        scn.nextLine(); // Used to consume the new line leftover from scn.nextDouble()
+        
         System.out.println("Equilateral Triangle Object's Name is " + eqTriangle.getName());
         eqTriangle.printDimensions();
         String eqTriangleArea = String.format("%.2f", eqTriangle.getArea());
