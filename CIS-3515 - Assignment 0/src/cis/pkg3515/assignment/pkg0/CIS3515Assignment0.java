16e523f11df06bdff781c4755bdc7e8d9170f0f8
Jsndng408 <54553870+Jsndng408@users.noreply.github.com>
2020-01-26 10:19:45 -0500

    Add files via upload
    Negative Input and 0 as an Input handling

--- CIS-3515 - Assignment 0/src/cis/pkg3515/assignment/pkg0/CIS3515Assignment0.java
+++ CIS-3515 - Assignment 0/src/cis/pkg3515/assignment/pkg0/CIS3515Assignment0.java
@@ -17,8 +17,22 @@ public class CIS3515Assignment0 {
         
         System.out.println("Enter the length of " + squareName);
         double length = scn.nextDouble();
+        
+        // Check for 0 or negative number Length, and set default Length to 1
+        if (length <= 0) {
+            length = 1;
+            System.out.println("Invalid Value. Setting " + squareName + "'s Length to " + length + ".");
+        }
+        
         System.out.println("Enter the height of " + squareName);
         double height = scn.nextDouble();
+        
+        // Check for 0 or negative number Height, and set default Height to 1
+        if (height <= 0) {
+            height = 1;
+            System.out.println("Invalid Value. Setting " + squareName + "'s Height to " + height + ".");
+        }
+        
         square.setDimensions(length, height);
         scn.nextLine(); // Used to consume the new line leftover from scn.nextDouble()
         
@@ -37,6 +51,13 @@ public class CIS3515Assignment0 {
         
         System.out.println("Enter the radius of " + circleName);
         double radius = scn.nextDouble();
+        
+        // Check for 0 or negative number Radius, and set default Radius to 1
+        if (radius <= 0) {
+            radius = 1;
+            System.out.println("Invalid Value. Setting " + circleName + "'s Radius to " + radius + ".");
+        }
+        
         circle.setDimensions(radius);
         scn.nextLine(); // Used to consume the new line leftover from scn.nextDouble()
         
@@ -54,10 +75,31 @@ public class CIS3515Assignment0 {
         
         System.out.println("Enter the length of the first side of " + triangleName);
         double side1 = scn.nextDouble();
+        
+        // Check for 0 or negative number for the First Side, and set default value of First Side to 1
+        if (side1 <= 0) {
+            side1 = 1;
+            System.out.println("Invalid Value. Setting " + triangleName + "'s first side length to " + side1 + ".");
+        }
+        
         System.out.println("Enter the length of the second side of " + triangleName);
         double side2 = scn.nextDouble();
+        
+        // Check for 0 or negative number for the Second Side, and set default value of Second Side to 1
+        if (side2 <= 0) {
+            side2 = 1;
+            System.out.println("Invalid Value. Setting " + triangleName + "'s second side length to " + side2 + ".");
+        }
+        
         System.out.println("Enter the length of the third side of " + triangleName);
         double side3 = scn.nextDouble();
+        
+        // Check for 0 or negative number for the Third Side, and set default value of Third Side to 1
+        if (side3 <= 0) {
+            side3 = 1;
+            System.out.println("Invalid Value. Setting " + triangleName + "'s third side length to " + side3 + ".");
+        }
+        
         triangle.setDimensions(side1, side2, side3);
         scn.nextLine(); // Used to consume the new line leftover from scn.nextDouble()
         
@@ -75,6 +117,13 @@ public class CIS3515Assignment0 {
         
         System.out.println("Enter the length of the side of " + eqTriangleName);
         double side = scn.nextDouble();
+        
+        // Check for 0 or negative number for the Side, and set default value of Side to 1
+        if (side <= 0) {
+            side = 1;
+            System.out.println("Invalid Value. Setting " + eqTriangleName + "'s side length to " + side + ".");
+        }
+        
         eqTriangle.setDimensions(side);
         scn.nextLine(); // Used to consume the new line leftover from scn.nextDouble()
         
@@ -84,7 +133,5 @@ public class CIS3515Assignment0 {
         System.out.println("Equilateral Triangle Object's Area = " + eqTriangleArea);
         
         System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-        
     }
-    
 }
